efad4549e979aae5827d599f7fec65c1f4d48356
David <yidavidh@outlook.com>
2023-03-26 20:04:15 -0700

    Lab complete

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(a,b) {
+    a = 2000;
+    b = 100;
+    return a/b;
+}
+
+const square =(x) => {
+    x = 2;
+    return x*x;
+}
+
+const add =(a,b) => {
+    a = 3;
+    b = 4;
+    return a+b;
+}
